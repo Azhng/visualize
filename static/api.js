bb1d89f236f0fa76ac5fcb42a1b0f536d4ae7cd1
Gerardo Torres <gerardo.torrescastro@cockroachlabs.com>
2022-03-17 17:41:35 -0400

    add sql text to nodes

--- static/api.js
+++ static/api.js
@@ -1,14 +1,50 @@
 const contentionEventAPI = "/api/_status/transactioncontentionevents"
 const translationAPI = "/getAggTs"
+const statementAPI = "api/_status/statements"
+
+async function getStatements() {
+    const resp = await fetch(statementAPI, {mode: 'cors'});
+    const data = await resp.json();
+
+    var stmtLookup = {};
+    var txns = data["transactions"];
+
+    txns.forEach(txn => {
+        var stmts = [];
+        var txnFingerprintId = txn["statsData"]["transactionFingerprintId"];
+        var stmtFingerprintIds = txn["statsData"]["statementFingerprintIds"];
+
+        stmtFingerprintIds.forEach(stmtFingerprintId => {
+            var query = getStatementSQLFromStmtFingerprintId(data, stmtFingerprintId)
+            stmts.push(query);
+        })
+        
+        stmtLookup[txnFingerprintId] = stmts;
+    })
+
+    return stmtLookup;
+}
+
+function getStatementSQLFromStmtFingerprintId(data, stmtFingerprintId) {
+    var stmts = data["statements"];
+    for (var i = 0; i < stmts.length; i++) {
+        var stmt = stmts[i];
+        
+        if (stmt["id"] === stmtFingerprintId) {
+            return stmt["key"]["keyData"]["query"];
+        }
+    }
+}
 
 async function getContentionEvents(main) {
     try {
         const resp = await fetch(contentionEventAPI, {mode: 'cors'})
         const data = await resp.json()
         const lookupTable = await getAggTsForTxnFingerprintIDs(data)
-        main(data, lookupTable)
+        const stmtLookup = await getStatements();
+        main(data, lookupTable, stmtLookup)
     } catch(e) {
-       alert(e);
+        alert(e);  
     }
 }
 
